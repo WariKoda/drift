9beddc9130742190d3fe34ada319946219cb22a9
Niklas Braun <niklasbraun123@gmail.com>
2026-04-18 18:21:31 +0200

    Improve version detection for go install builds

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -13,10 +15,43 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print drift version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("drift %s\n", Version)
+		fmt.Printf("drift %s\n", resolvedVersion())
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+}
+
+func resolvedVersion() string {
+	if Version != "" && Version != "dev" {
+		return Version
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return Version
+	}
+
+	if info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+
+	for _, setting := range info.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			if len(setting.Value) >= 7 {
+				return "dev-" + setting.Value[:7]
+			}
+			if setting.Value != "" {
+				return "dev-" + setting.Value
+			}
+		case "vcs.modified":
+			if strings.EqualFold(setting.Value, "true") {
+				return "dev-dirty"
+			}
+		}
+	}
+
+	return Version
 }
